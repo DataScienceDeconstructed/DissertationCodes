46b5df775b6ab2f3cc90dbd0d46213f0bb3ec845
chdavis <34408482+DataScienceDeconstructed@users.noreply.github.com>
2024-04-27 15:07:41 -0500

    checkpoint - Updating for adjusting gamma. updated the sim to warmup with high gamma and then process to completion with low gamma. Also removed the old code.

--- create_exp.sh
+++ create_exp.sh
@@ -102,7 +102,7 @@ else
 fi
 
 time_adjust="$mpd_dir/analysis/changeTime"
-gamma_adjust="$mpd_dir/changeGamma"
+gamma_adjust="$mpd_dir/bin/changeGamma"
 int_adjust="$mpd_dir/bin/changeIntervals"
 time_delta="$mpd_dir/analysis/changeDeltaT"
 
@@ -193,15 +193,29 @@ while IFS=' ' read -r line Uvalue radius aDen nanos; do
 
 		    #these are for testing
         #these have been changed to hurry up testing
-        $time_adjust $file_name 0 1000
-		    $int_adjust $file_name 100 10
+        #$time_adjust $file_name 0 1000
+		    #$int_adjust $file_name 100 10
+        #testing gamme
+        $gamma_adjust $filename 20.0
+        $time_adjust $file_name 0 10
+		    $int_adjust $file_name 10 10
 
 		    #copy the default basesim file into this directory, and update it for this simulation
 		    cp "$base_dir/basesim.sh" ./
 
         #echo "module load gcc/8.2.0" >> ./basesim.sh
+        # warm up system with high gamma
         echo "module load fftw/3.3.10/gcc-8.5.0/openmpi-4.1.6" >> ./basesim.sh
 		    echo "$mpd_dir/MD $file_name" >> ./basesim.sh #add processing to submission file
+
+		    #update system parameters and run to completion.
+		    echo "echo 'updating simulation gamma and time files' " >> ./basesim.sh
+		    echo "$gamma_adjust $filename 1.0" >> ./basesim.sh
+        echo "$time_adjust $file_name 0 1000" >> ./basesim.sh
+		    echo "$int_adjust $file_name 100 10" >> ./basesim.sh
+        echo "$mpd_dir/MD $file_name" >> ./basesim.sh #add processing to submission file
+
+
 		    echo "module load python/3.12.1/gcc.8.5.0" >> ./basesim.sh #add python for analysis to submission file
 		    echo "python3 $base_dir/main.py $sim_dir/ $file_name ">> ./basesim.sh # execute analyis on the file after simulation.
 
@@ -229,93 +243,3 @@ curl -d "text=Clayton Sims have all started in $exp_dir " -d "${slack[1]}" -H "$
 
 echo "exiting"
 exit 0
-
-# nanovalues=(64 128 256 320 384 512 768 1024)
-
-# The meat of our code is a set of 4 nested loops. The loops work together to create and start series of simulations.
-# The for loops are over Umin, np radius, polymer grafting density, and number of nanoparticle variables. The total
-# number of simulations is equal to NoI(Umin) * NoI(np radius) * NoI(grafting density) * NoI(nanoparticles) where NoI is
-# an operater that returns the number of intervals the for loops execute for each variable.
-
-#The outter most loop is over Umin values. The values are dimensionaless and seperated by -0.175. For each Umin value in the
-# loop execution, the loop calculates Umin values and then creates a subdirectory inside the experiment's directory.
-#Todo we are replacing the convoluted math and indexing with a file that has exactly what we want
-#for ((Uindex=1;Uindex<=2;Uindex +=1))
-#do
-#Umin_base=-0.175
-#Umin=`echo 0.0 + $Uindex*$Umin_base | bc`
-
-U_dir="$exp_dir/Umin_$Uindex"
-echo $U_dir
-mkdir $U_dir
-
-# The next loop is over the radius of the particles. Again it is dimensionless. the delta for the radius values is 2. For
-# each radius value a sum directory is created in the current Umin directory.
-#Todo replacing with file read
-#for((radius=2;radius<=4;radius +=2))
-#do
-
-#rad_flt=`echo $radius*1.0 | bc`
-rad_dir="$U_dir/rad_$radius"
-echo $rad_dir
-mkdir $rad_dir
-
-# the next loop is over the grafting density of the polymer brush in the simulation being crafted. for each value of grafting
-# density a subdirectory is created in the current radius directory.
-#Todo replace with file read
-#for(( aDen=1;aDen<=20;aDen++ ))
-#do
-#	area_den=`echo 0.0 + $aDen*.03 | bc`
-  den_dir="$rad_dir/den_$aDen"
-	echo $den_dir
-	mkdir $den_dir
-
-  # the innermost loop for the script progresses over different values of nanoparticles for the simulation. Originally the
-  # code used an algorithm that progressed according to 2^n where n was a value between 6 and 10; however, the exponential
-  # growth left large gaps in the recorded data so a list was create that better captures the nanparticle saturations of interest.
-  # for each value in the nanovalues list a directory is created inside the current grafting directory and a simulation is
-  # started with that directory as its base directory for placing outputs.
-	## Todo replace with file read approach
-	#for i in ${nanovalues[@]}
-	#do
-		##nan_num=$(($((2**$nanos))+$((2**8))))
-		#create the directory and filename base for simulation outputs
-	#	nan_num=$i
-		nanos=$nan_num
-		echo $nan_num
-		sim_dir="$den_dir/NP_$nanos"
-		echo $sim_dir
-		mkdir $sim_dir
-
-		cd $sim_dir # move into the simulation directory
-    file_name="$exp_name.Umin$Uindex.rad$radius.den$aDen.NP$nanos"
-
-		##echo "filename -> $file_name"
-		##echo "UMin -> $Umin"
-
-		# generate the files needed for the simulation and adjust default time variables
-		module load gcc/8.2.0
-		module load fftw/3.8.0
-		$gen_dir $file_name $RANDOM 800 $area_den $Umin 40 $nan_num $rad_flt 0 100 100 0.7 3.0
-		$time_adjust $file_name 0 100000
-		$int_adjust $file_name 1000 100
-
-		#copy the default basesim file into this directory, and update it for this simulation
-		cp "$base_dir/basesim.sh" ./
-
-    echo "module load gcc/8.2.0" >> ./basesim.sh
-    echo "module load fftw/3.8.0" >> ./basesim.sh
-		echo "$base_dir/MD $file_name" >> ./basesim.sh #add processing to submission file
-		echo "module load python/3.8.7" >> ./basesim.sh #add python for analysis to submission file
-		echo "python3 $base_dir/main.py $sim_dir/ $file_name ">> ./basesim.sh # execute analyis on the file after simulation.
-    #echo "" send alert that processing has completed here
-		# send the simulation off for processing to the cluster
-		sbatch ./basesim.sh
-
-		#move back to the pwd to prepare the next processing
-		cd "$base_dir"
-#	done
-#done
-
-#done
-#done
