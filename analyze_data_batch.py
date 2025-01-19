f964eb753750981ac28345ce5a90ac5efd9b3adf
chdavis <34408482+DataScienceDeconstructed@users.noreply.github.com>
2025-01-19 14:08:07 -0600

    restructuring code

--- analyze_data_batch.py
+++ analyze_data_batch.py
@@ -32,8 +32,11 @@ for root, dirs, files in os.walk(base_dir):
     print(root, dirs)
     error_list = {"no_files":[], "no_data":[]}
     path = root.split(os.sep)
+
+    #check to make sure we aren't in a known bad directory
     if ("35" in root.split("/")[-4]) or ("BRUSH" in root.split("/")[-1]):
         #ignore the -.35 Umins because of the PBC issue in the Z direction
+        #ignore Brush because of the NP_Brush directory
         continue
 
     #check to make sure you are in a directory with data.
@@ -43,7 +46,7 @@ for root, dirs, files in os.walk(base_dir):
         print("num NPs", num_NPs)
 
         dir_base = root
-        total += 1
+        total += 1  #number of directories with data
 
         # dist holds number of particles loaded in brush and floating in solvent at each time step
         dist = ReferenceDistribution(_type="Binary", _reference=0.0, _dist=[0, 0])
@@ -54,26 +57,32 @@ for root, dirs, files in os.walk(base_dir):
         info_lag = []
         # brushz_lag holds the brush height at each time step
         brushz_lag = []
+
         # bins for z axis profiling
         bin_length = 0.5  # this bin length is used to cut the system height into intervals for binning
         total_bins = int(1000.0 / bin_length)  # 1000 is used because it is a sim max. i.e. the system can only have a height of 1000
-        bin_values = np.asarray([x*bin_length  for x in range(total_bins)])
+        bin_values = np.asarray([x*bin_length  for x in range(total_bins)]) # z values for the bins
+
         # poly_profile holds the number of polymers at sectional slice
         poly_profile_lag = []
         poly_profile_current = np.zeros(total_bins, dtype=int)
         np_profile_lag = []
         np_profile_current = np.zeros(total_bins, dtype=int)
+
+        #hold average height based "top" calculation
         poly_avg_height_lag = []
         poly_avg_height_lag.append(0)
         tempheight = 0
         monocount = 0
+
         system_dimensions = [0.0, 0.0, 0.0]  # default values that will be overwritten by file data
 
         # retrieve radius from filename and calculate NP Volume. relies on naming conventions from create_exp.sh
-        radius = float(dir_base.split("/")[-3].split("_")[-1])
+
         sigma =  float(dir_base.split("/")[-2].split("_")[-1])
         print("sigma ", sigma)
 
+        radius = float(dir_base.split("/")[-3].split("_")[-1])
         print("radius ", radius)
         NP_Volume = 4.0 / 3.0 * np.pi * radius * radius * radius
         print("NP_Volume ", NP_Volume)
@@ -151,19 +160,14 @@ for root, dirs, files in os.walk(base_dir):
             np_profile_current = np.array(returndict["np_profile"])
             poly_profile_current = np.array(returndict["poly_profile"])
 
-            results_dir = "/post"
-            if not os.path.exists(dir_base + results_dir):
-                os.makedirs(dir_base + results_dir)
-
-
-            with open(dir_base + results_dir+ "/loading_solv.dat", 'w') as fp:
+            with open(dir_base + "/loading_solv.dat", 'w') as fp:
                 np.savetxt(fp, loading_array[:,1], fmt='%.6e', delimiter=' ', newline='\n', header=str(top), footer='', comments='# ',
                           encoding=None)
-            with open(dir_base + results_dir+ "/loading_brush.dat", 'w') as fp:
+            with open(dir_base + "/loading_brush.dat", 'w') as fp:
                 np.savetxt(fp, loading_array[:,0], fmt='%.6e', delimiter=' ', newline='\n', header=str(top), footer='', comments='# ',
                           encoding=None)
 
-            with open(dir_base + results_dir +"/z_profile.dat", 'w') as fp:
+            with open(dir_base + "/z_profile.dat", 'w') as fp:
                 np.savetxt(fp, np_profile_current, fmt='%.6e', delimiter=' ', newline='\n', header=str(top), footer='', comments='# ',
                           encoding=None)
 
@@ -174,7 +178,7 @@ for root, dirs, files in os.walk(base_dir):
             brush_top[int(top//bin_length)] += 1
             brush_top[int(system_dimensions[2]//bin_length)] += 1
             z_data = np.column_stack((bin_values, np_profile_current, poly_profile_current, brush_top))
-            with open(dir_base + results_dir + "/profile_data.dat", 'w') as fp:
+            with open(dir_base + "/profile_data.dat", 'w') as fp:
                 np.savetxt(fp, z_data, fmt='%.6e', delimiter=' ', newline='\n', header=str(top), footer='', comments='# ',
                           encoding=None)
 
